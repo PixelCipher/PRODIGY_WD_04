e04de7a58fe84e376760e892f33ae2f8cdb92866
PixelCipher <chandantarai1661@gmail.com>
2025-06-05 19:15:18 +0530

    files uploaded

---
+++ personal portfolio website/script.js
@@ -0,0 +1,41 @@
+// Side navigation
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let navBar = document.querySelector(".navbar");
+let body = document.querySelector("body");
+
+menuBtn.onclick = function () {
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  navBar.classList.add("active");
+  body.style.overflow = "hidden";
+};
+cancelBtn.onclick = function () {
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  navBar.classList.remove("active");
+  body.style.overflow = "auto";
+};
+
+// Sticky Navigation Menu 
+let nav = document.querySelector("nav");
+let val;
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+
+
+// side navigation menu bar close when navigations link clicked
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", () => {
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    navBar.classList.remove("active");
+    body.style.overflow = "auto";
+  });
+}
